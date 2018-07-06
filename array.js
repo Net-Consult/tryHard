3f1a00da922933a7a71cabd71464a48fc0ba362c
Ghjuvan <vaevconsult@gmail.com>
2018-07-06 13:51:01 +0200

    Update array.js

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-const data = [
+const hostels = [
     {
         id: 1,
         name: 'hotel rose',
@@ -193,3 +193,16 @@ console.log('data', data);
 
 // exercice 2 : faire un tableau avec toutes les chambres de tous les hotels, et ne garder que les chambres qui
 // ont plus que ou 3 places et les classer par ordre alphabétique selon le non de la chambre
+
+// exercice 3 : mettre une majuscule à tous les mots qui sont dans l'attribut RoomName
+
+
+// exercice 4 : enlever toutes les chambres qui ont plus de 3 places et changer la valeur de roomNumbers pour qu'elle reflete
+// le nouveau nombre de chambres
+
+// exercice 5  : extraire du tableau hostels l'hotel qui a le nom 'hotel ocean' en le supprimant du tableau, et le mettre dans une nouvelle variable
+// puis effacer toutes ses chambres et mettre à jour sa valeur room number, puis pusher l'hotel modifié dans hostel, puis faire un sort par nom d'hotel
+// puis donner le nouvel index de l'hotel océan
+
+// exercice 6 : créer un objet dont les clés sont le nom des hotels et dont la valeur est un booléen qui indique si l'hotel a une chambre qui s'appelle 'suite marseillaise'
+
