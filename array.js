37373c79ffddda45c2feb8fd416108c1cd2233aa
ghjuvan <sebastien@net-consult.info>
2019-05-06 17:51:00 +0200

    add for test

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-const hostels = [
+let hostels = [
     {
         id: 1,
         name: 'hotel rose',
@@ -187,7 +187,6 @@ const hostels = [
 ];
 
 
-
 // exercice 1 : trier les hotels par nombre de chambres (plus grand en 1er) et créer un tableau contenant seulement
 // le nom des hotels dans leur ordre de tri
 
@@ -196,17 +195,25 @@ const hostels = [
 // ont plus que 3 places ou exactement 3 places et les classer par ordre alphabétique selon le non de la chambre
 
 
-
-
 // exercice 3 : mettre une majuscule à tous les mots qui sont dans l'attribut RoomName
 
 
 // exercice 4 : enlever toutes les chambres qui ont plus de 3 places et changer la valeur de roomNumbers pour qu'elle reflete
 // le nouveau nombre de chambres
 
+
 // exercice 5  : extraire du tableau hostels l'hotel qui a le nom 'hotel ocean' en le supprimant du tableau, et le mettre dans une nouvelle variable
 // puis effacer toutes ses chambres et mettre à jour sa valeur room number, puis pusher l'hotel modifié dans hostel, puis faire un sort par nom d'hotel
 // puis donner le nouvel index de l'hotel océan (faire 2 méthodes : avec indexOf et avec un foreach)
 
+
 // exercice 6 : créer un objet dont les clés sont le nom des hotels et dont la valeur est un booléen qui indique si l'hotel a une chambre qui s'appelle 'suite marseillaise'
+
+
+
+
+
+
+
+
 
---
+++ for.js
@@ -0,0 +1,12 @@
+// exercice 1 : générer un array avec 100 objets avec la forme ci dessous, dont les données sont toutes aléatoires
+const person = {firstName : '', lastName: '', age : 0};
+// bonus : calculer la moyenne des âges de personnes en utilisant un reduce
+
+
+// exercice 2 : générer un tableau contenant des nombres pairs consécutifs, le premier nombre du tableau doit être 4,
+// on doit arreter de remplir le tableau quand il y a 20 nombres pairs dans le tableau
+
+
+// exercice 3 : Écrire un programme qui affiche les nombres de 1 à 199 avec un console log.
+// Mais pour les multiples de 3, afficher “Fizz” au lieu du nombre et pour les multiples de 5 afficher “Buzz”.
+// Pour les nombres multiples de 3 et 5, afficher “FizzBuzz”.
