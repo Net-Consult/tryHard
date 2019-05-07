952feb97c183ee6941b64e3a76ca8b2231e0b254
Sarida Taibet <taibetsarida@hotmail.fr>
2019-05-07 17:11:40 +0200

    finish exo 1, 2, 3

--- array.js
+++ array.js
@@ -225,10 +225,7 @@ hostels.map(hostel=> {
                 .map(word => word.charAt(0).toUpperCase() + word.slice(1))
                 .join (' ')})
         return hostel
-
     }
-
-
 
     );
 console.log(hostels);*/
@@ -236,22 +233,75 @@ console.log(hostels);*/
 // exercice 4 : enlever toutes les chambres qui ont plus de 3 places et changer la valeur de roomNumbers pour qu'elle reflete
 // le nouveau nombre de chambres
 
+/*const result3 = hostels
+
+    hostels.map(hostel=> {
+            hostel.rooms = hostel.rooms.filter(room=>(room.size<=3));
+                    return hostels;
+})
+ hostels.map(hostel=>{hostel.roomNumbers = hostel.room});
+
+console.log(hostels);*/
 
 // exercice 5  : extraire du tableau hostels l'hotel qui a le nom 'hotel ocean' en le supprimant du tableau, et le mettre dans une nouvelle variable
 // puis effacer toutes ses chambres et mettre à jour sa valeur room number, puis pusher l'hotel modifié dans hostel, puis faire un sort par nom d'hotel
 // puis donner le nouvel index de l'hotel océan (faire 2 méthodes : avec indexOf et avec un foreach)
 
+/*const hotels = hostels.filter(
+    hostel=> hostel.name ==='hotel ocean'
+);
+
+              hostels.splice ('OceanIndex',1);*/
+
+
+
+ /*hostels.map((hostels)=>{
+        return(hostel.rooms-splice(0,15))});*/
+
+
+/*.map = hostel.room=[];
+                    hostels.roomNumbers=hostels.splice;
+                        hostels.push(ocean);
+                        hostels.sort((hostel1, hostel2)=> hostel1.name? -1:1);*/
+
+
+
+/*console.log(hotels);*/
+
 // exercice 6 : créer un objet dont les clés sont le nom des hotels et dont la valeur est un booléen qui indique si l'hotel a une chambre qui s'appelle 'suite marseillaise'
 
 
+     
+ } (name, hostels) {
+            var inputs = $('hostels').map(function () {
+
+                var key = this.attr('name'),
+                value = this.attr('value');
+
+            })
+
+
+
+    
+}
+const hostel = {
+
+    suiteMarseillaise: true,
+
+    printIntroduction: function () {
+        console.log(`hostels is ${this.suiteMarseillaise}. hostel? $ {.room}`)
+
+    }
+};
+
+console.log(hostel);
+/*const hostel1 = hostel.create(hostels)*/
 
 
 
 
 
-
-
-const tabPrenom = ['Lucie', 'George', 'luc', 'Kris', 'Dave', 'Marie', 'Elena'];
+/*const tabPrenom = ['Lucie', 'George', 'luc', 'Kris', 'Dave', 'Marie', 'Elena'];
 
 
 const tabNom = ['Petit','Thomas','Michel','Roussel', 'Simon','Bailly','Langlois'];
@@ -283,11 +333,10 @@ for (let i=0; people.length<100; i=i+1){
 
 };
 
-console.log(people)
+console.log(people);
 
 /*math.floor(math.random()*10);
 console.log(math.floor(math.random)*10);*/
-
 
 
 
