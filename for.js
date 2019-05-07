193a6e95376b1ba55cc2c12cb1461d4ae52ce798
Vaniina <anna.vessereau@gmail.com>
2019-05-07 17:37:36 +0200

    exercice 2

--- for.js
+++ for.js
@@ -2,7 +2,7 @@
 //const person = {firstName: '', lastName: '', age: 0};
 // bonus : calculer la moyenne des âges de personnes en utilisant un reduce
 
-const people = [];
+/*const people = [];
 const firstName = ['Pierre', 'Paul', 'Jacques', 'Marion', 'Audrey', 'Louise', 'Vincent', 'Jéremy', 'Amélie', 'Julia']
 const lastName = ['Dupond', 'Durand', 'Marceau', 'Martinez', 'Thibault', 'Truffaut', 'Beaugrand', 'Galmier', 'Dumontier', 'Blanc']
 
@@ -23,13 +23,20 @@ const averageAge = people
     / people.length;
 
 const average = parseFloat(averageAge.toFixed(2));
-
-console.log(average);
+console.log(average);*/
 
 
 // exercice 2 : générer un tableau contenant des nombres pairs consécutifs, le premier nombre du tableau doit être 4,
 // on doit arreter de remplir le tableau quand il y a 20 nombres pairs dans le tableau
 
+const tab = [];
+
+for(let i = 4; tab.length < 20; i++) {
+        if(i % 2 === 0) {
+            tab.push(i)
+    }
+}
+console.log(tab);
 
 // exercice 3 : Écrire un programme qui affiche les nombres de 1 à 199 avec un console log.
 // Mais pour les multiples de 3, afficher “Fizz” au lieu du nombre et pour les multiples de 5 afficher “Buzz”.
