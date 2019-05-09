cce0a5707696a4556e033760a4302c864a260832
ibrahimaSy3 <syibrahima972@gmail.com>
2019-05-09 16:40:14 +0200

    add new documentation

---
+++ array.md
@@ -0,0 +1,7 @@
+
+Array
+-----
+
+
+L'objet global Array est utilisé pour créer des tableaux. Les tableaux sont des objets de haut-niveau (en termes de complexité homme-machine) semblables à des listes.
+
--- for.js
+++ for.js
@@ -1,6 +1,73 @@
 // exercice 1 : générer un array avec 100 objets avec la forme ci dessous, dont les données sont toutes aléatoires
+/*
 const person = {firstName : '', lastName: '', age : 0};
-// bonus : calculer la moyenne des âges de personnes en utilisant un reduce
+//bonus : calculer la moyenne des âges de personnes en utilisant un reduce
+
+
+const firstName= ['tata','toto','snow' ,'blanc','philippe','potter','malefoy','londubat','granger','weasley'];
+const lastName= ['rita','lilio','john','laurent','edouard','harry' ,'drago', 'neville', 'hermione','ron'];
+
+function generatefirstName() {
+
+    return firstName [Math.floor(Math.random()*10)]
+}
+
+function generateLastName() {
+    return lastName [Math.floor(Math.random() * 10)]
+
+
+}function generateAge(){
+    return Math.floor(Math.random()*10)
+
+}
+
+
+console.log( generateLastName());
+console.log(generatefirstName());
+console.log(generateage());
+
+const people = [ ];
+for (let i = 1; people.length <100; i++) {
+}
+const personne = {
+    firstName : generatefirstName(),
+    lastName : generateLastName(),
+    age: generateage()
+
+};
+   people.push(person);
+}
+console.log(people);
+
+const sum = people.reduce((accumulator,currentValue)=> {
+    return accumulator+currentValue.age
+
+
+},0)
+
+
+const moyenne=sum/person.length;
+
+console.log (sum);
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
 
 
 // exercice 2 : générer un tableau contenant des nombres pairs consécutifs, le premier nombre du tableau doit être 4,
@@ -10,3 +77,4 @@ const person = {firstName : '', lastName: '', age : 0};
 // exercice 3 : Écrire un programme qui affiche les nombres de 1 à 199 avec un console log.
 // Mais pour les multiples de 3, afficher “Fizz” au lieu du nombre et pour les multiples de 5 afficher “Buzz”.
 // Pour les nombres multiples de 3 et 5, afficher “FizzBuzz”.
+
