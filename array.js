fdaebd24f69e00fe3365bac5800ef32658f56800
LaurentJeanZephirin <jeanzephirin93@gmail.com>
2019-05-09 18:31:02 +0200

    exo op

--- array.js
+++ array.js
@@ -190,30 +190,81 @@ let hostels = [
 // exercice 1 : trier les hotels par nombre de chambres (plus grand en 1er) et créer un tableau contenant seulement
 // le nom des hotels dans leur ordre de tri
 
+/*const trie = hostels.sort((hostel1, hostel2) => hostel2.roomNumbers - hostel1.roomNumbers)
+    .map(hostel => hostel.name);
+
+console.log(trie);*/
+
 
 // exercice 2 : faire un tableau avec toutes les chambres de tous les hotels, et ne garder que les chambres qui
 // ont plus que 3 places ou exactement 3 places et les classer par ordre alphabétique selon le non de la chambre
 
 
+/*const tab = hostels.reduce((acc, hostel) => acc.concat(hostel.rooms),[])
+    .filter(room => room.size >= 3).map(name => {name.roomName; return name}).sort();
+
+console.log(tab);*/
+
+
 // exercice 3 : mettre une majuscule à tous les mots qui sont dans l'attribut RoomName
+
+
+/*hostels = hostels.map(hostel => {
+    hostel.rooms.map(room =>
+        room.roomName = room.roomName
+            .split(' ')
+            .map(chName => chName.charAt(0).toLocaleUpperCase() + chName.slice(1))
+            .join(' '));
+    return hostel
+});
+
+console.log(hostels);*/
 
 
 // exercice 4 : enlever toutes les chambres qui ont plus de 3 places et changer la valeur de roomNumbers pour qu'elle reflete
 // le nouveau nombre de chambres
 
 
+/*hostels = hostels
+    .map(hostel => { hostel.rooms = hostel.rooms
+        .filter(room => room.size >= 3);
+            return hostel
+    }).map(hostel => { hostel.roomNumbers = hostel.rooms.length;
+        return hostel;
+});
+
+console.log(hostels);*/
+
 // exercice 5  : extraire du tableau hostels l'hotel qui a le nom 'hotel ocean' en le supprimant du tableau, et le mettre dans une nouvelle variable
 // puis effacer toutes ses chambres et mettre à jour sa valeur room number, puis pusher l'hotel modifié dans hostel, puis faire un sort par nom d'hotel
 // puis donner le nouvel index de l'hotel océan (faire 2 méthodes : avec indexOf et avec un foreach)
 
+/*const index = hostels.findIndex(hostel => hostel.name === 'hotel ocean');
+
+const [hostel] = hostels.splice(index, 1);
+
+hostel.rooms = [];
+
+hostel.roomNumbers = hostel.rooms.length;
+
+hostels.push(hostel);
+
+hostels.sort((hostell1 , hostell2) => hostell2.name > hostell1.name ? -1 : 1);
+
+const nIndex = hostels.indexOf(hostel);
+
+
+
+console.log(nIndex);*/
+
 
 // exercice 6 : créer un objet dont les clés sont le nom des hotels et dont la valeur est un booléen qui indique si l'hotel a une chambre qui s'appelle 'suite marseillaise'
 
+/*let object = {};
 
+hostels.forEach(hostel => object[hostel.name] = hostel.rooms.filter(hostel => hostel.roomName === 'suite marseillaise').length > 0);
 
-
-
-
+console.log(object);*/
 
 
 
--- for.js
+++ for.js
@@ -1,12 +1,67 @@
-// exercice 1 : générer un array avec 100 objets avec la forme ci dessous, dont les données sont toutes aléatoires
+/*// exercice 1 : générer un array avec 100 objets avec la forme ci dessous, dont les données sont toutes aléatoires
 const person = {firstName : '', lastName: '', age : 0};
 // bonus : calculer la moyenne des âges de personnes en utilisant un reduce
 
+const firstName = ['Loic', 'Brice', 'Anna', 'Ibrahima', 'Sebastien', 'Alina', 'Morgane', 'Bender', 'Fry', 'Léo', 'Président'];
+const lastName = ['Charo', 'Loup', 'Vautour', 'Cannibale', 'Chouette', 'Canaille', 'Tricard', 'Fripouille', 'Bandit', 'Chicken', 'Pard'];
+
+function generateName() {
+    return firstName[Math.floor(Math.random()*11)];
+}
+
+function generateLastName() {
+    return lastName[Math.floor(Math.random()*11)];
+}
+
+function age() {
+    return Math.floor(Math.random() * 100 + 1);
+}
+
+let persons = [];
+
+
+for (let i = 0; persons.length < 50; i++) {
+    const personne = {
+        firstName: generateName(),
+        lastName: generateLastName(),
+        age: age()
+    };
+    persons.push(personne)
+}
+
+const moyenAge = persons
+    .reduce((acc, currentValue) => {return acc + currentValue.age / persons.length},0);
+
+console.log(persons);
+console.log(moyenAge);*/
 
 // exercice 2 : générer un tableau contenant des nombres pairs consécutifs, le premier nombre du tableau doit être 4,
 // on doit arreter de remplir le tableau quand il y a 20 nombres pairs dans le tableau
 
+/*let tab = [];
+
+for (let i = 4; tab.length < 20; i++) {
+    if (i % 2 === 0) {
+        tab.push(i);
+    }
+}
+
+console.log(tab);*/
 
 // exercice 3 : Écrire un programme qui affiche les nombres de 1 à 199 avec un console log.
 // Mais pour les multiples de 3, afficher “Fizz” au lieu du nombre et pour les multiples de 5 afficher “Buzz”.
 // Pour les nombres multiples de 3 et 5, afficher “FizzBuzz”.
+
+
+/*for (let i = 1; i < 200; i++) {
+    if (i % 3 == 0 && !(i % 5 == 0)) {
+        console.log('Fizz');
+    } else if (i % 5 == 0 && !(i % 3 == 0)) {
+        console.log('Buzz');
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
+    } else {
+        console.log(i);
+    }
+}*/
+
