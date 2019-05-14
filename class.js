295a943bc9e36f7db87252797a21b7a8f8fc09a3
mehdiLegoullon <legoullonmehdi@gmail.com>
2019-05-14 12:18:33 +0200

    update loop for for the exercice class.js

--- class.js
+++ class.js
@@ -1,21 +1,53 @@
 
+
+const id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const size = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const hostelRoomName = ['suite parisienne', 'suite lilloise', 'suite marseillaise', 'suite bordelaise', 'suite lyonnaise',
+    'suite niçoise', 'suite de perpignan', 'suite brestoise', 'suite nantaise', 'suite de aliens'];
+
+
+
 class Room {
 
-    id;
-    size;
-    roomName;
+    addId;
+    addSize;
+    addRoomName;
 
-    constructor(id, size, roomName) {
-        this.id = id;
-        this.size = size;
-        this.roomName = roomName;
+    constructor(addId, addSize, addRoomName) {
+        Room.addId = addId;
+        Room.addSize = addSize;
+        Room.addRoomName = addRoomName;
     }
 
-    pushTab() {
 
+    addId() {
+        return id;
     }
+
+    addSize() {
+        return size[Math.floor(Math.random() * 10)];
+    }
+
+    addRoomName() {
+        return hostelRoomName[Math.floor(Math.random() * 10)];
+    }
+
+/*    static addGenerateRooms() {
+
+        let rooms = [];
+
+        for (let i = 0; rooms.length < 10; i++) {
+
+            const newRooms = {
+                id: Room.addId(),
+                size: Room.addSize(),
+                roomName: Room.addRoomName()
+            };
+
+            rooms.push(newRooms);
+        }
+    }*/
 }
 
-const tabRoom = [];
-
-console.log(tabRoom);
+const result = new Room(addId, addSize, addRoomName);
+console.log(result);
