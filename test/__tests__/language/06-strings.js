e57be0c804773a8018d0e2003e4d09ba3b4f6d3b
Vaniina <anna.vessereau@gmail.com>
2019-05-16 14:02:50 +0200

    done strings

--- test/__tests__/language/06-strings.js
+++ test/__tests__/language/06-strings.js
@@ -3,30 +3,30 @@ describe("Strings", () => {
     // prettier-ignore
     var singleQuotedString = 'apple';
     var doubleQuotedString = "apple";
-    expect(singleQuotedString === doubleQuotedString).toBe(_);
-    expect(typeof singleQuotedString).toBe(_);
-    expect(typeof doubleQuotedString).toBe(_);
+    expect(singleQuotedString === doubleQuotedString).toBe(true);
+    expect(typeof singleQuotedString).toBe('string');
+    expect(typeof doubleQuotedString).toBe("string");
   });
 
   it("Strings - 2", () => {
     var fruit = "apple";
     var dish = "pie";
-    expect(_).toBe("apple pie."); // Use concatenation
-    expect(_).toBe("apple pie."); // Use template strings
+    expect(fruit + ' ' + dish + ".").toBe("apple pie."); // Use concatenation
+    expect(`${fruit} ${dish}.`).toBe("apple pie."); // Use template strings
   });
 
   it("Strings - 3", () => {
     var characterType = typeof "Amory".charAt(1);
-    expect(characterType).toBe(_);
+    expect(characterType).toBe("string");
   });
 
   it("Strings - 4", () => {
     var result = "hello";
-    expect(result.length).toBe(_);
+    expect(result.length).toBe(5);
   });
 
   it("Strings - 5", () => {
     var fruit = "pineapples";
-    expect(fruit.slice(_, _)).toBe("apple");
+    expect(fruit.slice(4, 9)).toBe("apple");
   });
 });
