90e732a1ac89f782ead9f024d84554c76ed1a1da
SaridaTaibet <taibetsarida@hotmail.fr>
2019-06-03 17:11:30 +0200

    exo test

--- test/__tests__/language/02-coercion.js
+++ test/__tests__/language/02-coercion.js
@@ -1,41 +1,41 @@
 describe("Coercion", () => {
   it("Coercion - 1", () => {
-    expect(3 + 7 === 10).toBe(_);
+    expect(3 + 7 === 10).toBe(true);
   });
 
   it("Coercion - 2", () => {
-    expect(3 + 7 == "10").toBe(_);
+    expect(3 + 7 == "10").toBe(true);
   });
 
   it("Coercion - 3", () => {
-    expect(1 == true).toBe(_);
+    expect(1 == true).toBe(true);
   });
 
   it("Coercion - 4", () => {
-    expect(new Date(3) + 7 == "10").toBe(_);
+    expect(new Date(3) + 7 == "10").toBe(false);
   });
 
   it("Coercion - 5", () => {
-    expect("3" + 7 == "37").toBe(_);
+    expect("3" + 7 == "37").toBe(true);
   });
 
   it("Coercion - 6", () => {
-    expect(["3"] + ["7"] == 37).toBe(_);
+    expect(["3"] + ["7"] == 37).toBe(true);
   });
 
   it("Coercion - 7", () => {
-    expect(typeof (/3/ + 7)).toBe(_);
+    expect(typeof (/3/ + 7)).toBe("string");
   });
 
   it("Coercion - 8", () => {
-    expect(1 + true === "1true").toBe(_);
+    expect(1 + true === "1true").toBe(false);
   });
 
   it("Coercion - 9", () => {
-    expect(+"3" + +"7" == "37").toBe(_);
+    expect(+"3" + +"7" == "37").toBe(false);
   });
 
   it("Coercion - 10", () => {
-    expect("3" + "7" == 37).toBe(_);
+    expect("3" + "7" == 37).toBe(true);
   });
 });
